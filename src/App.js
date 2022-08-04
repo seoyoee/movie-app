8500253a3245583b750af5d124384923f466ec65
syoake <ryupark1006@gmail.com>
2022-08-05 01:50:06 +0900

    food app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({name, picture}) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image: 'https://m.unlimga.com/web/product/medium/202204/15fb42e1dec2d997bc18dad0a764f4d8.jpg',
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image: 'https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg',
+  },
+  {
+    id: 3,
+    name: 'Bibimbop',
+    image: 'https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/S9Y/image/76pr0EfQi6R21MMIyzefN9hiqIU.jpg',
+  },
+  {
+    id: 4,
+    name: 'Donkatsu',
+    image: 'https://t1.daumcdn.net/cfile/tistory/9951473F5D4633FD2C',
+  },
+  {
+    id: 5,
+    name: 'Kimbop',
+    image: 'https://img.hankyung.com/photo/202110/99.27136316.1.jpg',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
